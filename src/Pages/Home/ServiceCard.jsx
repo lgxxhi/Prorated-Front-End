19460177bd2ba879509ebb85994a5d6d08db374c
Yanako Lajara <yanakolajara@pursuit.org>
2023-11-02 12:38:46 -0400

    Background modified

--- src/Pages/Home/ServiceCard.jsx
+++ src/Pages/Home/ServiceCard.jsx
@@ -2,18 +2,13 @@ import React from 'react'
 
 export default function ServiceCard({data}) {
   const { name, description, status, image } = data;
+  const background = {backgroundImage: `url(${image})`}
   return (
-    <div className='serviceCard'>
-
-      <div className='serviceCard__info'>
-        <p>{name}</p>
-        <div className='serviceCard__info__descriptionDropdown'>
+    <div className='serviceCard' style={background}>
+        <p className='serviceCard__title'>{name}</p>
+        <div className='serviceCard__info'>
           <p>{description}</p>
         </div>
-      </div>
-      
-        <img src={image} alt="" className='serviceCard__image'/>
-
     </div>
   )
 }
