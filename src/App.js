d6bd0b0aa5bfb2b01dc036aa238cec270fccc83e
lgxxhi <luisgsilva95@gmail.com>
2023-11-02 19:37:18 -0400

    added contractor cards, contractor dropdwon, contractor listings page frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContractorDetails from "./Components/ContractorDetails/ContractorDetails";
+import ContractorListings from "./Components/ContractorListings/ContractorListings";
+import contractorData from "./contractorData.json";
 import "./Styles/Global.scss";
 import Nav from "./Components/Navbar";
 import Home from "./Pages/Home/Home";
@@ -14,6 +17,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<ContractorDetails />} />
           <Route path="/login-signup" element={<LoginSignup />} />
+          <Route
+            path="/listings"
+            element={<ContractorListings contractorData={contractorData} />}
+          />
         </Routes>
       </Router>
     </div>
---
+++ src/components/ContractorListings/ContractorListings.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./ContractorListings.css";
+
+import Dropdown from "../Dropdown/Dropdown";
+import ContractorCard from "../ContractorCard/ContractorCard";
+import FilterBar from "../FilterBar/FilterBar";
+
+function ContractorListings(contractorData) {
+  return (
+    <div className="container">
+      <nav>Nav</nav>
+
+      <div className="main">
+        <div className="main-header">
+          <div className="left-main">
+            <h2>
+              <span className="results-for">Results For</span>
+              <span className="service-found"> Plumbing</span>
+            </h2>
+            <p>{contractorData.contractorData.length} services available </p>
+          </div>
+
+          <Dropdown />
+        </div>
+
+        <div className="main-content">
+          <ContractorCard />
+        </div>
+      </div>
+
+      {/* <div className="sidebar">
+        <header>ProRated</header>
+        <FilterBar />
+      </div> */}
+    </div>
+  );
+}
+
+export default ContractorListings;
