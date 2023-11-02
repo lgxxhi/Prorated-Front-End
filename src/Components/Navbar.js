25020f0785837774ec733c732cf25bf7d07bf6c8
daleskiii <aun2793@gmail.com>
2023-11-02 12:31:55 -0400

    navbar-contractordetails pushed up

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ContractorDetails from "../Pages/ContractorDetails";
+
 import { Link } from "react-router-dom";
 function Nav() {
   return (
--- src/Pages/ContractorDetails.js
+++ src/Pages/ContractorDetails.js
@@ -1,14 +1,34 @@
 import React from "react";
 
 function ContractorDetails() {
-  return (
-    <>
-      <h1>Contractor Profile</h1>
+  // Sample data for a contractor
+  const contractor = {
+    name: "John Doe",
+    jobTitle: "Electrician",
+    location: "New York, NY",
+    experience: "5 years",
+    contact: "john.doe@email.com",
+    phoneNumber: "555-123-4567",
+    description:
+      "John Doe is an experienced electrician with a passion for providing high-quality electrical services...",
+  };
 
+  return (
+    <div>
+      <h2>Contractor Profile</h2>
       <div>
-        <p>description</p>
+        <h3>{contractor.name}</h3>
+        <p>Job Title: {contractor.jobTitle}</p>
+        <p>Location: {contractor.location}</p>
+        <p>Experience: {contractor.experience}</p>
+        <p>Contact: {contractor.contact}</p>
+        <p>Phone Number: {contractor.phoneNumber}</p>
       </div>
-    </>
+      <div>
+        <h4>About {contractor.name}</h4>
+        <p>{contractor.description}</p>
+      </div>
+    </div>
   );
 }
 
