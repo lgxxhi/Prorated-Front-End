9d02c29165b931690f117d765889690f41fbb4ef
Yanako Lajara <yanakolajara@pursuit.org>
2023-11-02 12:19:27 -0400

    Service card structure

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,32 +1,46 @@
 import React from 'react'
 import '../../Styles/Global.scss'
 import SearchBar from '../../Components/SearchBar'
+import ServiceCard from './ServiceCard'
 
 export default function homePage() {
 
+  const contractorImage = require('../../Assets/Images/contractor-homepage-img.png')
+  const defaultServices = require('../../Assets/services.json')
+
   const displayServices = () => {
-    return(
-    <>
-      <p>
-        display cards here
-      </p>
-    </>)
+    return defaultServices.map(({name,description,status,image}) => {
+      return(
+        <>
+          <ServiceCard
+          data={{
+            name: name,
+            description: description,
+            status: status,
+            image: image
+          }}
+          />
+        </>
+      )
+    })
   }
 
   return (
     <div className='home-page'>
       <div className='home-page__search'>
-        <h1>Building trust</h1>
-        <h1>One project at a time</h1>
-        <SearchBar location="home-page"/>
-        <div className='home-page__search__suggestions'>
-          <p>Popular services:</p>
-          <button className='btn btn__medium-round'>Plumbing</button>
-          <button className='btn btn__medium-round'>Electrician</button>
-          <button className='btn btn__medium-round'>Handyman</button>
+        <div className='home-page__search__input'>
+          <h1>Building trust</h1>
+          <h1>One project at a time</h1>
+          <SearchBar location="home-page"/>
+          <div className='home-page__search__suggestions'>
+            <p>Popular services:</p>
+            <button className='btn btn__medium-round'>Plumbing</button>
+            <button className='btn btn__medium-round'>Electrician</button>
+            <button className='btn btn__medium-round'>Handyman</button>
+          </div>
         </div>
+        <img src={contractorImage} alt="Contractor and client" className='home-page__search__img'/>
       </div>
-
       <div className='home-page__popular-services'>
         {displayServices()}
       </div>
--- src/Pages/Home/ServiceCard.jsx
+++ src/Pages/Home/ServiceCard.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export default function ServiceCard() {
+export default function ServiceCard({data}) {
+  const { name, description, status, image } = data;
   return (
-    <div>ServiceCard</div>
+    <div className='serviceCard'>
+
+      <div className='serviceCard__info'>
+        <p>{name}</p>
+        <div className='serviceCard__info__descriptionDropdown'>
+          <p>{description}</p>
+        </div>
+      </div>
+      
+        <img src={image} alt="" className='serviceCard__image'/>
+
+    </div>
   )
 }
