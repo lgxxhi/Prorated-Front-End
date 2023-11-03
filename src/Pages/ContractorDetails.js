ef5779ae33bd16e064beaedfb547dbddb063d051
daleskiii <aun2793@gmail.com>
2023-11-03 14:08:36 -0400

    new changes to commit to new branch

--- src/Pages/ContractorDetails.js
+++ src/Pages/ContractorDetails.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function ContractorDetails() {
-  // Sample data for a contractor
   const contractor = {
     name: "John Doe",
     jobTitle: "Electrician",
@@ -11,6 +10,24 @@ function ContractorDetails() {
     phoneNumber: "555-123-4567",
     description:
       "John Doe is an experienced electrician with a passion for providing high-quality electrical services...",
+
+    profileImageSrc: "path_to_image.jpg",
+
+    pastJobs: [
+      {
+        title: "Job 1",
+        reviews: ["Great job! Very professional.", "Excellent workmanship."],
+      },
+      {
+        title: "Job 2",
+        reviews: ["Highly recommended.", "Outstanding service."],
+      },
+    ],
+    servicesOffered: [
+      "Electrical Wiring",
+      "Fixture Installation",
+      "Troubleshooting",
+    ],
   };
 
   return (
@@ -18,6 +35,7 @@ function ContractorDetails() {
       <h2>Contractor Profile</h2>
       <div>
         <h3>{contractor.name}</h3>
+        <img src={contractor.profileImageSrc} alt={contractor.name} />
         <p>Job Title: {contractor.jobTitle}</p>
         <p>Location: {contractor.location}</p>
         <p>Experience: {contractor.experience}</p>
@@ -25,8 +43,32 @@ function ContractorDetails() {
         <p>Phone Number: {contractor.phoneNumber}</p>
       </div>
       <div>
-        <h4>About {contractor.name}</h4>
+        <h3>About {contractor.name}</h3>
         <p>{contractor.description}</p>
+      </div>
+      <div>
+        <h3>Past Jobs</h3>
+        <ul>
+          {contractor.pastJobs.map((job, index) => (
+            <li key={index}>
+              {job.title}
+              <ul>
+                {job.reviews.map((review, reviewIndex) => (
+                  <li key={reviewIndex}>{review}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h3>Services Offered</h3>
+        <ul>
+          {contractor.servicesOffered.map((service, index) => (
+            <li key={index}>{service}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
