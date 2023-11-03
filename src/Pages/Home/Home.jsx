a2dc270a1922e97fde02835c89212f130de637b4
Yanako Lajara <yanakolajara@pursuit.org>
2023-11-03 14:33:04 -0400

    Home page design improved

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -29,17 +29,15 @@ export default function homePage() {
     <div className='home-page'>
       <div className='home-page__search'>
         <div className='home-page__search__input'>
-          <h1>Building trust</h1>
-          <h1>One project at a time</h1>
+          <h1>Building trust <br/> One project at a time</h1>
           <SearchBar location="home-page"/>
           <div className='home-page__search__suggestions'>
             <p>Popular services:</p>
-            <button className='btn btn__medium-round'>Plumbing</button>
+            <button className='btn btn__full-round'>Plumbing</button>
             <button className='btn btn__medium-round'>Electrician</button>
             <button className='btn btn__medium-round'>Handyman</button>
           </div>
         </div>
-        <img src={contractorImage} alt="Contractor and client" className='home-page__search__img'/>
       </div>
       <div className='home-page__popular-services'>
         {displayServices()}
