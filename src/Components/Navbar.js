cb12a4f34f24eeea4674f3762df45735306986fe
Yanako Lajara <yanakolajara@pursuit.org>
2023-11-03 02:20:50 -0400

    Navbar basic layout

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../Styles/Global.scss";
 
-import { Link } from "react-router-dom";
 function Nav() {
+  const logo = require("../Assets/Images/ProRatedLogo.png");
   return (
-    <nav>
-      <>
-        <Link to="/">Home</Link>
-        <Link to="/profile">Contractor Profile</Link>
-      </>
+    <nav className="navbar">
+      <div className="navbar__menu">
+        <img src={logo} alt="logo" className="navbar__menu__logo" />
+        <Link to="/" className="navbar__menu__button">
+          Home
+        </Link>
+        <Link to="/profile" className="navbar__menu__button">
+          Contractor Profile
+        </Link>
+      </div>
     </nav>
   );
 }
