30050de58eae3003719eb1157bd5d66954735ec4
michael reyes <draftiercrayon4@gmail.com>
2023-11-03 12:17:23 -0400

    completed basic login form setup

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import ContractorDetails from "./Pages/ContractorDetails";
 import "./Styles/Global.scss";
 import Nav from "./Components/Navbar";
 import Home from "./Pages/Home/Home";
+import LoginSignup from "./Pages/LogInSignup";
 
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<ContractorDetails />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<ContractorDetails />} />
+          <Route path="/login-signup" element={<LoginSignup />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
