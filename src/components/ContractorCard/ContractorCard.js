30d93cb90e1899f9a359231379c8825bcac27b07
lgxxhi <luisgsilva95@gmail.com>
2023-11-06 17:13:46 -0500

    Added hover for servoces added

--- src/components/ContractorCard/ContractorCard.js
+++ src/components/ContractorCard/ContractorCard.js
@@ -1,150 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./ContractorCard.css";
 
-function ContractorCard() {
+import ServicesOffered from "../ServicesOffered/ServicesOffered";
+
+function ContractorCard(contractorData) {
+  console.log(contractorData.contractorData.contractorData);
+
+  const [contractorListings, setContractorListings] = useState(
+    contractorData.contractorData.contractorData
+  );
+
+  function handleStars(rating) {
+    let rounded = Math.round(rating / 0.5) * 0.5;
+
+    return rounded;
+  }
+
   return (
     <div className="contractor-card">
-      <div className="profile-card">
-        <div className="profile-split">
-          <div className="image">
-            <img className="profile-img" src="https://i.pravatar.cc/300" />
-          </div>
-          <div className="card-content">
-            <div className="text-data">
-              <span className="name">I Handy Biz Inc.</span>
-            </div>
-            <div className="ratings">
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star-half"></i>
-              <span className="review-count">
-                <small>(21)</small>
-              </span>
-            </div>
-            <div className="hires">
-              <span>
-                <i class="bx bx-trophy"></i>
-                <small> 54 hires on ProRated</small>
-              </span>
-              <span>
-                <i class="bx bx-been-here"></i>
-                <small>
-                  <span className="bold">Area:</span> Manhattan, Brooklyn
-                </small>
-              </span>
-            </div>
-          </div>
-        </div>
+      {contractorListings.length > 0
+        ? contractorListings.map((item) => {
+            return (
+              <div key={item.id} className="profile-card">
+                <div className="profile-split">
+                  <div className="image">
+                    <img className="profile-img" src={item.img} />
+                  </div>
+                  <div className="card-content">
+                    <div className="text-data">
+                      <span className="name">{item.business_name}</span>
+                    </div>
 
-        <div className="card-side">
-          <div className="contact">
-            <span>
-              <i class="bx bx-message-rounded"></i>
-            </span>
-            <span>
-              <small> Contact For Price</small>
-            </span>
-          </div>
-          <button className="more-details">More Details</button>
-        </div>
-      </div>
-      <div className="profile-card">
-        <div className="profile-split">
-          <div className="image">
-            <img className="profile-img" src="https://i.pravatar.cc/300" />
-          </div>
-          <div className="card-content">
-            <div className="text-data">
-              <span className="name">I Handy Biz Inc.</span>
-            </div>
-            <div className="ratings">
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star-half"></i>
-              <span className="review-count">
-                <small>(21)</small>
-              </span>
-            </div>
-            <div className="hires">
-              <span>
-                <i class="bx bx-trophy"></i>
-                <small> 54 hires on ProRated</small>
-              </span>
-              <span>
-                <i class="bx bx-been-here"></i>
-                <small>
-                  <span className="bold">Area:</span> Manhattan, Brooklyn
-                </small>
-              </span>
-            </div>
-          </div>
-        </div>
+                    <div className="ratings">
+                      {handleStars(item.rating)}
+                      <i className="bx bxs-star"></i>
+                      <i className="bx bxs-star"></i>
+                      <i className="bx bxs-star"></i>
+                      <i className="bx bxs-star"></i>
+                      <i className="bx bxs-star"></i>
+                      <i className="bx bxs-star-half"></i>
+                      <span className="review-count">
+                        <small>({item.reviews})</small>
+                      </span>
+                    </div>
+                    <div className="hires">
+                      <span>
+                        <i className="bx bx-trophy"></i>
+                        <small> 54 hires on ProRated</small>
+                      </span>
+                      <span>
+                        <i className="bx bx-been-here"></i>
+                        <small>
+                          <span className="bold">Area:</span> Manhattan,
+                          Brooklyn
+                        </small>
+                      </span>
+                    </div>
+                  </div>
+                </div>
 
-        <div className="card-side">
-          <div className="contact">
-            <span>
-              <i class="bx bx-message-rounded"></i>
-            </span>
-            <span>
-              <small> Contact For Price</small>
-            </span>
-          </div>
-          <button className="more-details">More Details</button>
-        </div>
-      </div>
-      <div className="profile-card">
-        <div className="profile-split">
-          <div className="image">
-            <img className="profile-img" src="https://i.pravatar.cc/300" />
-          </div>
-          <div className="card-content">
-            <div className="text-data">
-              <span className="name">I Handy Biz Inc.</span>
-            </div>
-            <div className="ratings">
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star"></i>
-              <i class="bx bxs-star-half"></i>
-              <span className="review-count">
-                <small>(21)</small>
-              </span>
-            </div>
-            <div className="hires">
-              <span>
-                <i class="bx bx-trophy"></i>
-                <small> 54 hires on ProRated</small>
-              </span>
-              <span>
-                <i class="bx bx-been-here"></i>
-                <small>
-                  <span className="bold">Area:</span> Manhattan, Brooklyn
-                </small>
-              </span>
-            </div>
-          </div>
-        </div>
+                <div className="card-side">
+                  <div className="contact">
+                    <span>
+                      <i className="bx bx-message-rounded"></i>
+                    </span>
+                    <span>
+                      <small> Contact For Price</small>
+                    </span>
+                  </div>
+                  <button className="more-details">More Details</button>
+                </div>
+                <div className="services">
+                  <h2 className="show">
+                    <i class="bx bx-chevron-down"></i>
+                  </h2>
 
-        <div className="card-side">
-          <div className="contact">
-            <span>
-              <i class="bx bx-message-rounded"></i>
-            </span>
-            <span>
-              <small> Contact For Price</small>
-            </span>
-          </div>
-          <button className="more-details">More Details</button>
-        </div>
-      </div>
+                  <div className="hide">
+                    <ServicesOffered />
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 }
---
+++ src/components/ServicesOffered/ServicesOffered.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./ServicesOffered.css";
+
+function ServicesOffered() {
+  return (
+    <div className="grid-container">
+      <div className="grid-item grid-item-1">1</div>
+      <div className="grid-item grid-item-2">2</div>
+      <div className="grid-item grid-item-3">3</div>
+      <div className="grid-item grid-item-4">4</div>
+      <div className="grid-item grid-item-4">4</div>
+    </div>
+  );
+}
+
+export default ServicesOffered;
