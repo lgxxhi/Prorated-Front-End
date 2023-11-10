a00cdbb892b2900a213ee557b3147e826db22c25
daleskiii <aun2793@gmail.com>
2023-11-10 11:47:48 -0500

    new modification to contractor-profile- also css file added for modication from design professionals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ContractorDetails from "./Pages/ContractorDetails";
+import ContractorDetails from "./Components/ContractorDetails/ContractorDetails";
 import "./Styles/Global.scss";
 import Nav from "./Components/Navbar";
 import Home from "./Pages/Home/Home";
