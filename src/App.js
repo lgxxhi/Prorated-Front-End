7fd64c58d890dee705dcb5617ae2ad5fc0154a03
Yanako Lajara <yanakolajara@pursuit.org>
2023-11-14 12:50:30 -0500

    feature: created footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ContractorDetails from "./Pages/ContractorDetails";
 import "./Styles/Global.scss";
 import Nav from "./Components/Navbar";
 import Home from "./Pages/Home/Home";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<ContractorDetails />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../Styles/Global.scss";
+import logo from "../Assets/Images/ProRatedLogo.png";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <img src={logo} alt="" className="footer__logo" />
+      <div className="footer__routes">
+        <a className="footer__routes__button" href="/">
+          Home
+        </a>
+        <a className="footer__routes__button" href="/">
+          Dashboard
+        </a>
+        <a className="footer__routes__button" href="/">
+          Account
+        </a>
+        <a className="footer__routes__button" href="/">
+          About
+        </a>
+        <a className="footer__routes__button" href="/">
+          Contact
+        </a>
+      </div>
+    </footer>
+  );
+}
