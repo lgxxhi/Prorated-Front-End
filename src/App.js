d490acda0e61b353aa900f4ef15ec51c3e488163
Yanako Lajara <yanakolajara@pursuit.org>
2023-11-14 19:25:52 -0500

    fix: added contractor details route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ContractorDetails from "./Pages/ContractorDetails";
 import "./Styles/Global.scss";
 import Nav from "./Components/Navbar";
 import Home from "./Pages/Home/Home";
+import ContractorDetails from "./Pages/ContractorDetails";
 
 function App() {
   return (
