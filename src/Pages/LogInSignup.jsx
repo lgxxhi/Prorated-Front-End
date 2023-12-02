76866616287b0339dbe138b73c1e77bbaacbf75f
lgxxhi <luisgsilva95@gmail.com>
2023-12-01 22:29:58 -0500

    added useContext to import from react

--- src/Pages/LogInSignup.jsx
+++ src/Pages/LogInSignup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ContractorsContext } from "../context/ContractorsContext";
