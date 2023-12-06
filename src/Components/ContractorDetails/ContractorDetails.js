d93f4b8f130184932b52cdb08618fa95aa83a950
daleskiii <aun2793@gmail.com>
2023-12-06 12:22:23 -0500

    new changes added

--- src/Components/ContractorDetails/ContractorDetails.js
+++ src/Components/ContractorDetails/ContractorDetails.js
@@ -27,7 +27,10 @@ function ContractorDetails() {
   return (
     <div className="contractor-profile-container">
       <div className="container-details">
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <div
+          className="profile-header"
+          style={{ display: "flex", alignItems: "center" }}
+        >
           <div className="image">
             <img className="profile-img" src="https://picsum.photos/200/300" />
           </div>
@@ -43,15 +46,28 @@ function ContractorDetails() {
             </div>
           </div>
         </div>
-        <p>Location: {contractorProfile.location}</p>
-        <p>Experience: {contractorProfile.experience} years</p>
-        <p>Contact: {contractorProfile.contact}</p>
-        <p>Phone Number: {contractorProfile.phone_number}</p>
+        <div className="contact-info-description">
+          <p>
+            <b>Location</b>: {contractorProfile.location}
+          </p>
+          <p>
+            <b>Experience</b>: {contractorProfile.experience} years
+          </p>
+          <p>
+            {" "}
+            <b> Contact </b> : {contractorProfile.contact}
+          </p>
+          <p>
+            <b> Phone Number </b>: {contractorProfile.phone_number}
+          </p>
+        </div>
       </div>
-      <div>
+      <hr />
+      <div className="description">
         <h3>About {contractorProfile.name}</h3>
         <p>{contractorProfile.description}</p>
       </div>
+      <hr />
       <div className="past-jobs">
         <h3>Past Jobs</h3>
 
@@ -61,6 +77,7 @@ function ContractorDetails() {
           <p>No past jobs available</p>
         )}
       </div>
+      <hr />
       <ContractorReviewDetails />
       <button
         onClick={() =>
