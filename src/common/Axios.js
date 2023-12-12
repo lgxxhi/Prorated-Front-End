8a7db68218ae470070ef8e9a04efc5a02af29c28
Michael Reyes <122540844+MichaelReyesGit@users.noreply.github.com>
2023-12-12 11:26:33 -0500

    updated axios call

--- src/common/Axios.js
+++ src/common/Axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const AxiosInstance = axios.create({
   baseURL:
-    process.env.NODE_ENV === "development" ? "http://localhost:3001" : "",
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3001"
+      : process.env.REACT_APP_API_KEY,
   timeOut: 5000,
 });
 
